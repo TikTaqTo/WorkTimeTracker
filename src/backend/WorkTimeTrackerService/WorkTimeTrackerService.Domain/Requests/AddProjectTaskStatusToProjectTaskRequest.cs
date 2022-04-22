4b6450fb5854fc6a6cd061a2ebba7e95af74fd6b
Adai Ejechan <adai.02_kz@mail.ru>
2022-04-22 13:01:42 +0600

    Created models for business model binding requests: Project-ProjectTask, ProjectTask-ProjectTaskStatus, ProjectTask-ProjectTaskType

---
+++ src/backend/WorkTimeTrackerService/WorkTimeTrackerService.Domain/Requests/AddProjectTaskStatusToProjectTaskRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WorkTimeTrackerService.Domain.Requests
+{
+  public class AddProjectTaskStatusToProjectTaskRequest
+  {
+    public Guid ProjectTaskId { get; set; }
+    public Guid ProjectTaskStatusId { get; set; }
+  }
+}
