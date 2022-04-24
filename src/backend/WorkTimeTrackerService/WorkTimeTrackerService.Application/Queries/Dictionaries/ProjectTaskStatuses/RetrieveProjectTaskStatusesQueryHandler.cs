2f66e6d0659a1e7d7f53e0bd372e2ef676d3ca98
Adai Ejechan <adai.02_kz@mail.ru>
2022-04-24 18:27:45 +0600

    Added get request: ProjectTaskStatus and ProjectTaskStatuses

---
+++ src/backend/WorkTimeTrackerService/WorkTimeTrackerService.Application/Queries/Dictionaries/ProjectTaskStatuses/RetrieveProjectTaskStatusesQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WorkTimeTrackerService.Application.Abstractions.Dictionaries;
+using WorkTimeTrackerService.Domain.Replies.Dictionaries;
+
+namespace WorkTimeTrackerService.Application.Queries.Dictionaries.ProjectTaskStatuses
+{
+  public class RetrieveProjectTaskStatusesQueryHandler : IRequestHandler<RetrieveProjectTaskStatusesQuery, ProjectTaskStatusesReply>
+  {
+    private readonly IProjectTaskStatusService _projectTaskStatusService;
+
+    public RetrieveProjectTaskStatusesQueryHandler(IProjectTaskStatusService projectTaskStatusService)
+    {
+      _projectTaskStatusService = projectTaskStatusService;
+    }
+
+    public async Task<ProjectTaskStatusesReply> Handle(RetrieveProjectTaskStatusesQuery request, CancellationToken cancellationToken)
+    {
+      return await _projectTaskStatusService.RetrieveProjectTaskStatuses();
+    }
+  }
+}
