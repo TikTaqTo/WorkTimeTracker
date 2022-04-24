ff8691d31464fbdc33bdba151f1cd46a8acb56f0
Adai Ejechan <adai.02_kz@mail.ru>
2022-04-24 18:48:27 +0600

    Added middleware for all command validations. Basic implementation

---
+++ src/backend/WorkTimeTrackerService/WorkTimeTrackerService.Application/Behaviors/ValidatorBehavior.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkTimeTrackerService.Application.Behaviors
+{
+  public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+  {
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+      _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    {
+      //Проверить можно ли отказатся от условия if (_validators.Any()) так как у нас всегда есть валидаторы
+      if (_validators.Any())
+      {
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+        var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+        if (failures.Any())
+          throw new ValidationException("Validation exception", failures);
+      }
+      return await next();
+    }
+  }
+}
