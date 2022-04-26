8c83db248a00343fdd8dbe08e0c906aa74b1d94f
Adai Ejechan <adai.02_kz@mail.ru>
2022-04-26 12:28:29 +0600

    The Project Task Status entity has been added. This class differs from the Domain class in that it does not contain fields: List<ProjectTask> ProjectTasks.

---
+++ src/backend/WorkTimeTrackerService/WorkTimeTrackerService.Api.Model/EntityModels/Dictionaries/ProjectTaskStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WorkTimeTrackerService.Api.Model.EntityModels.Dictionaries
+{
+  public class ProjectTaskStatus : BaseEntity<Guid>
+  {
+    [Required]
+    [StringLength(200)]
+    public string Status { get; set; }
+  }
+}
