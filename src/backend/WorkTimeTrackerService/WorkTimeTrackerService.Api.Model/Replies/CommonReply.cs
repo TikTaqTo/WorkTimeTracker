f739bea25d06c8a12afe275c8c4864e987db9098
TikTaqTo <adai.02_kz@mail.ru>
2022-06-13 19:21:29 +0600

    Created a basic, common reply for our business entities.

---
+++ src/backend/WorkTimeTrackerService/WorkTimeTrackerService.Api.Model/Replies/CommonReply.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WorkTimeTrackerService.Api.Model.Replies
+{
+  public class CommonReply
+  {
+    public IEnumerable<string> Errors { get; set; }
+    public IEnumerable<string> Warnings { get; set; }
+  }
+}
