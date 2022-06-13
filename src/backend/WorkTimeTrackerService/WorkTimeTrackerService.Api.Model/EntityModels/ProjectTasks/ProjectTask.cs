b549bd3dad5a0fa8bdfdd366e462c4f0fb2e6202
TikTaqTo <adai.02_kz@mail.ru>
2022-06-13 19:06:02 +0600

    The Project Task model is created, the project dependency is missing at the moment, but it will be added in the next commit

---
+++ src/backend/WorkTimeTrackerService/WorkTimeTrackerService.Api.Model/EntityModels/ProjectTasks/ProjectTask.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WorkTimeTrackerService.Api.Model.EntityModels.Dictionaries;
+
+namespace WorkTimeTrackerService.Api.Model.EntityModels.ProjectTasks
+{
+  public class ProjectTask : BaseEntity<Guid>
+  {
+    [Required]
+    [StringLength(200)]
+    public string Title { get; set; }
+
+    [StringLength(1000)]
+    public string Description { get; set; }
+
+    public string Comment { get; set; }
+
+    public bool IsComplete { get; set; }
+
+    public DateTimeOffset taskStartAt { get; set; }
+    
+    public DateTimeOffset taskEndAt { get; set; }
+
+    [Required]
+    public Guid ProjectId { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(ProjectId))]
+    #nullable disable
+    public virtual Project Project { get; set; }
+
+    public Guid? TaskTypeId { get; set; }
+
+    [ForeignKey(nameof(TaskTypeId))]
+    #nullable enable
+    public virtual ProjectTaskType? TaskType { get; set; }
+
+    public Guid? TaskStatusId { get; set; }
+
+    [ForeignKey(nameof(TaskStatusId))]
+    #nullable enable
+    public virtual ProjectTaskStatus? TaskStatus { get; set; }
+  }
+}
